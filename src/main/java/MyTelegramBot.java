411ce418f895cd2f2849675c9d5f6f30aabb996b
Yona <krutikov_ivan_romanovich@mail.ru>
2023-10-02 23:05:15 +0500

    Ветка для новой структуры бота

--- src/main/java/MyTelegramBot.java
+++ src/main/java/MyTelegramBot.java
@@ -5,6 +5,17 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyTelegramBot extends TelegramLongPollingBot {
     @Override
+    public String getBotUsername() {
+        // SportsTimetableBot
+        return "CurrencyExchangerProjectBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6416187841:AAHmmO5gFJ_UERT5LNrQdxXb6dNJZaAKlts";
+        //6521790062:AAHBfHm6N_EZFjrZbkBTKejJZnPMmlwPsyk
+    }
+    @Override
     public void onUpdateReceived(Update update) {//Здесь всё что мы получаем от юзера при общении с ботом
         //System.out.println(update.getMessage().getText());// Получаем сообщения которые вводят боту
         //System.out.println(update.getMessage().getFrom().getFirstName());// Получаем ник того кто вводит
@@ -15,24 +26,11 @@ public class MyTelegramBot extends TelegramLongPollingBot {
             SendMessage response = new SendMessage();
             response.setChatId(update.getMessage().getChatId().toString());
             response.setText(message);
-
             try {
                 execute(response);
             } catch (TelegramApiException E){
                 E.printStackTrace();
             }
         }
-    }
-
-    @Override
-    public String getBotUsername() {
-        // TODO
-        return "CurrencyExchangerProjectBot";
-    }
-
-    @Override
-    public String getBotToken() {
-        // TODO
-        return "6416187841:AAHmmO5gFJ_UERT5LNrQdxXb6dNJZaAKlts";
     }
 }
