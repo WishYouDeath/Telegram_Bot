b0083754b5abea6ad8417777bf98c9b396f28d89
Yona <krutikov_ivan_romanovich@mail.ru>
2023-10-06 22:36:37 +0500

    Закинул все данные в переменные окружения

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
-    //add comment
     public static void main(String [] args){
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
