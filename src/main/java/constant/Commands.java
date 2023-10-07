fb2827480027bc792e3ac6afed9b0501b95fdde0
Yona <krutikov_ivan_romanovich@mail.ru>
2023-10-08 02:18:59 +0500

    Изменил структуру бота, добавил команды

---
+++ src/main/java/constant/Commands.java
@@ -0,0 +1,8 @@
+package constant;
+
+public class Commands {
+    public static final String START = "/start";
+    public static final String HELP = "/help";
+    public static final String ABOUT = "/about";
+    public static final String AUTHORS = "/authors";
+}
