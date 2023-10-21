251f75e90ed5ec97d36528fb4bc40f7085d7d106
Yona <krutikov_ivan_romanovich@mail.ru>
2023-10-21 16:54:29 +0500

    Работает по запросу user'a не сильно оптимизирован, сделали состояние диалгоа

--- src/main/java/MyTelegramBot.java
+++ src/main/java/MyTelegramBot.java
@@ -10,9 +10,11 @@ import java.util.Optional;
 import java.util.function.Consumer;
 
 import static constant.Commands.*;
+import constant.DialogState;
 public class MyTelegramBot extends TelegramLongPollingBot {
     SendMessageOperationCreate sendMessageOperationCreate = new SendMessageOperationCreate();
     Map<String, Consumer<Message>> commandMap = new HashMap<>();
+    private Map<Long, DialogState> dialogStateMap = new HashMap<>();
     public void addCommands(){
         commandMap.put(START, message -> executeMessage(sendMessageOperationCreate.createGreetingInformation(message)));
         commandMap.put(HELP, message -> executeMessage(sendMessageOperationCreate.createHelpInformation(message)));
@@ -28,6 +30,29 @@ public class MyTelegramBot extends TelegramLongPollingBot {
     }
     private void handleMessage(Message message) {
         addCommands();
+        if (dialogStateMap.containsKey(message.getChatId()) && dialogStateMap.get(message.getChatId()) == DialogState.WAITING_FOR_TEAM_NAME) {
+            handleGetCommand(message);
+        }
+        if (message.hasText() && message.hasEntities()) {
+            Optional<MessageEntity> commandEntity =
+                    message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
+
+            if (commandEntity.isPresent()) {
+                String command = message.getText().substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
+
+                if (command.equals(GET)) {
+                    dialogStateMap.put(message.getChatId(), DialogState.WAITING_FOR_TEAM_NAME);
+                    executeMessage(sendMessageOperationCreate.createSimpleMessage(message, "Введите название команды (teamName):"));
+                } else {
+                    commandMap.getOrDefault(command, msg -> executeMessage(sendMessageOperationCreate.wrongCommand(message))).accept(message);
+                }
+            } else if (dialogStateMap.containsKey(message.getChatId()) && dialogStateMap.get(message.getChatId()) == DialogState.WAITING_FOR_TEAM_NAME) {
+                handleGetCommand(message);
+            }
+        }
+    }
+    /*private void handleMessage(Message message) {
+        addCommands();
         if (message.hasText() && message.hasEntities()) {
             Optional<MessageEntity> commandEntity =
                     message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
@@ -37,9 +62,19 @@ public class MyTelegramBot extends TelegramLongPollingBot {
                 commandMap.getOrDefault(command, msg -> executeMessage(sendMessageOperationCreate.wrongCommand(message))).accept(message);
             }
         }
+    }*/
+    private void handleGetCommand(Message message) {
+        if (message.hasText()) {
+            String teamName = message.getText();
+            Parser parser = new Parser();
+            executeMessage(sendMessageOperationCreate.getTimeTable(message, teamName));
+            dialogStateMap.remove(message.getChatId()); // Удаляем состояние диалога после завершения запроса
+        } else {
+            executeMessage(sendMessageOperationCreate.createSimpleMessage(message, "Пожалуйста, введите название команды (teamName):"));
+        }
     }
 
-    private void handleGetCommand(Message message) {
+    /*private void handleGetCommand(Message message) {
         // Ожидаем следующее сообщение пользователя с именем команды (teamName)
         // Для этого нужно проверить, есть ли следующее сообщение и является ли оно текстовым
         System.out.println("yes");
@@ -54,7 +89,7 @@ public class MyTelegramBot extends TelegramLongPollingBot {
         } else {
             executeMessage(sendMessageOperationCreate.createSimpleMessage(message, "Пожалуйста, введите название команды (teamName):"));
         }
-    }
+    }*/
     String nameBot = System.getenv("Telegram_Name");
     String apiBot = System.getenv("Telegram_API");
     @Override
